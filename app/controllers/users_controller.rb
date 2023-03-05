8eba20afbd5204f9fadd5c90d606ee805a12c730
rezakumara2023 <rezakumara2023@u.northwestern.edu>
2023-03-05 20:07:03 +0000

    v1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+  
   def new
     @user = User.new
   end
@@ -11,6 +15,6 @@ class UsersController < ApplicationController
     @user["email"] = params["user"]["email"]
     @user["password"] = params["user"]["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user["id"]}"
   end
 end
