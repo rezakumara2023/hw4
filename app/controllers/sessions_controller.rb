e29cf54af577dfee65490a05808aafd04c227e9e
rezakumara2023 <rezakumara2023@u.northwestern.edu>
2023-03-05 21:02:53 +0000

    v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # authenticate the user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if @user["password"] == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]} you are logged in."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope., please enter the right password"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["username"] = params["user"]["username"]
     @user["first_name"] = params["user"]["first_name"]
     @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
