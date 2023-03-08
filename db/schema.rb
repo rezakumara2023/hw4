e121a084df52c32b73188a862e0deb861cb3b248
rezakumara2023 <rezakumara2023@u.northwestern.edu>
2023-03-08 01:49:46 +0000

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_211457) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "username"
     t.string "email"
     t.string "password"
